abdcc7840a82a935b47be5e2463e23a9a63b7744
Bex <rebeccaruva@newschool.edu>
2021-12-13 16:24:15 +0100

    color selector modification

--- colorselectorwidget.cpp
+++ colorselectorwidget.cpp
@@ -1,24 +1,48 @@
 #include "colorselectorwidget.h"
+#include "pixelcanvas.h"
 
 #include <QDebug>
-#include <QDesktopWidget>
+#include <QLabel>
+#include <QHBoxLayout>
 #include <QColorDialog>
 #include <QPushButton>
 
 ColorSelectorWidget::ColorSelectorWidget(QWidget* parent) :
-    QWidget(parent) {
-    QDesktopWidget dw;
+    QWidget(parent)
+{
+    QWidget* colorSelectorWidget = new QWidget(this);
+
+    button = new QPushButton;
+    button->setFixedSize(QSize(62, 62));
+
+    colorLabel = new QLabel();
+
+
+    updateButtonColor(Qt::white);
+
+    QHBoxLayout* colorsArea = new QHBoxLayout(colorSelectorWidget);
+    colorsArea->addWidget(button);
+    colorsArea->addWidget(colorLabel);
     this->setMinimumHeight(100); //set min height of color selector
-    //transparent icon, change color of button when color is selected
-    //    QPushButton *button = new QPushButton;
-//    button->setIcon(QIcon(":/icons/..."));
-//    button->setIconSize(QSize(65, 65));
+
+    connect(button,SIGNAL(clicked()),this,SLOT(selectColor()));
 }
 
 void ColorSelectorWidget::selectColor() {
     QColor color = QColorDialog::getColor(Qt::white, this );
     if (color.isValid()){
         qDebug() << "Color Choosen : " << color.name();
-        //change color of button and update the selected color on pixelCanvas
+        updateButtonColor(color.name());
+        //update the selected color on pixelCanvas
+        pixelCanvas->changeSelectedColor(color);
     }
+}
+
+void ColorSelectorWidget::updateButtonColor(QColor color) {
+    //update button color
+    m_buttonStyle = QString("background-color: %1").arg(color.name());
+    button->setStyleSheet(m_buttonStyle);
+
+    //update corresponding label
+    colorLabel->setText(QString("%1").arg(color.name()));
 }
--- colorselectorwidget.h
+++ colorselectorwidget.h
@@ -1,8 +1,11 @@
 #ifndef COLORSELECTORWIDGET_H
 #define COLORSELECTORWIDGET_H
 
+#include "pixelcanvas.h"
 #include <QWidget>
 #include <QVBoxLayout>
+#include <QLabel>
+#include <QPushButton>
 #include <QColorDialog>
 
 class ColorSelectorWidget : public QWidget
@@ -10,9 +13,15 @@ class ColorSelectorWidget : public QWidget
     Q_OBJECT
 public:
     ColorSelectorWidget(QWidget* parent = nullptr);
+    PixelCanvas *pixelCanvas;
+private slots:
     void selectColor();
 private:
     QVBoxLayout *m_colorsLayout;
+    QString m_buttonStyle;
+    QPushButton *button;
+    QLabel *colorLabel;
+    void updateButtonColor(QColor);
 };
 
 #endif // COLORSELECTORWIDGET_H
